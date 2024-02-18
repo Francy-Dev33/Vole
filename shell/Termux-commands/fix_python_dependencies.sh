c6b725a0bcdee937b6fb71b8ecd5be465c4594ea
Francy-Dev33 <francescoaulicino39@gmail.com>
2024-02-18 11:25:45 +0000

    add commands fix Python3 pip pygame

---
+++ shell/Termux-commands/fix_python_dependencies.sh
@@ -0,0 +1,26 @@
+pip install tk
+pip3 install pygame
+pkg install tur-repo
+pkg install python-pygame
+pip install --upgrade pip
+pip install requests
+pip install --upgrade setuptools wheel
+pip install requests --upgrade
+pip install numpy -vvv
+apt update
+sudo apt-get install python3-pygame
+
+
+# This command working on Termux no Ish shell etc 
+
+
+
+
+
+
+
+
+
+
+
+
